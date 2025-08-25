3e7c6f1afc4e5df7dedadda083b326ef95a6aca8
SM ALAMIN HOSSAIN <engrsmalaminhossain@gmail.com>
2025-08-25 09:31:24 +0800

    githup connect and login page and home page complete

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// login button functionality 
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault()
+    const mobileNumber = 17544231170;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+    if(mobileNumber === mobileNumberValueConverted && pinNumber === pinNumberValueConverted){
+        window.location.href = "./home.html";
+    }
+    else{
+        alert('Invalid credentials')
+    }
+})
